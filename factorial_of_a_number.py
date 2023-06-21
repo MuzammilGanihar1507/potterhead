667b67294f10dd10e461bc90675191e7a9a307e1
Muzammil Ganihar <119493054+MuzammilGanihar1507@users.noreply.github.com>
2023-06-21 09:53:56 +0530

    Create factorial_of_a_number.py
    Finding a factorial of a number.

---
+++ factorial_of_a_number.py
@@ -0,0 +1,14 @@
+# Python program to find the factorial of a number provided by the user
+# using recursion
+
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+num = int(input("Enter a number: "))
+result = factorial(num)
+print("The factorial of", num, "is", result)
